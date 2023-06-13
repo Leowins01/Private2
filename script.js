d0ea15b8f155700074179e06c0c5d3d6f365c007
Leonardo Gonçalves <126135841+Leowins01@users.noreply.github.com>
2023-06-12 23:14:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleContent() {
+    var element = document.getElementById("tudo");
+    if (element.style.display === "none") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  }
